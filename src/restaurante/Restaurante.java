b8e37154703f63601e6d3c46589e7fb50e389bda
unknown <takedi006@gmail.com>
2024-08-15 14:52:48 -0300

    Tentamos likar com banco de dados

--- src/restaurante/Restaurante.java
+++ src/restaurante/Restaurante.java
@@ -2,8 +2,13 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
+
+
 package restaurante;
 
+import java.sql.Connection; 
+import java.sql.DriverManager;
+import java.sql.SQLException;
 /**
  *
  * @author CAMARGO
@@ -13,8 +18,27 @@ public class Restaurante {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-        // TODO code application logic here
-    }
-    
+   public static String status = "Não conectou...";
+       
+       	public Restaurante() {     }
+        
+        public static java.sql.Connection getConexaoMySQL() {
+        Connection connection = null;    //atributo do tipo Connection
+        return connection;
+        
+        
+        String driverName = "Banco_Finalizado";                  
+try {
+	Class.forName(driverName);
+} catch (ClassNotFoundException e) {
+	e.printStackTrace();
 }
+        }
+        
+        
+}
+
+
+
+
+
